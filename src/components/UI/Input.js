b0417e534ebdb6a8f6a48da9a2a4d4a291e48b02
apjavier2 <apjavier27@gmail.com>
2023-02-28 09:52:19 +0800

    added adding item to cart logic

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,6 +1,7 @@
 import classes from "./Input.module.css";
+import React from "react";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
@@ -8,9 +9,9 @@ const Input = (props) => {
       e.g.
       {type:"text"} -> type="text"
       */}
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 
 export default Input;
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,13 +1,40 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    //concat generates a new array
+    const updatedItems = state.items.concat(action.item);
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return { items: updatedItems, totalAmount: updatedTotalAmount };
+  } else if (action.type === "REMOVE_ITEM") {
+  }
+  return defaultCartState;
+};
 
 //this component can be used to wrap components that needs access to the cart
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemFromCartHandler = (id) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
+  };
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE_ITEM", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
