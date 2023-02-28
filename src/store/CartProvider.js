142435a40b12a62a1e0a3f90b54dcf4584c3023d
apjavier2 <apjavier27@gmail.com>
2023-02-28 10:39:51 +0800

    added logic for avoiding duplicates in cart list

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,10 +8,30 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD_ITEM") {
-    //concat generates a new array
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    //This will return item index if the item id is already in the cart
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItem;
+    let updatedItems;
+
+    if (existingCartItem) {
+      updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      //concat generates a new array
+      updatedItems = state.items.concat(action.item);
+    }
+
     return { items: updatedItems, totalAmount: updatedTotalAmount };
   } else if (action.type === "REMOVE_ITEM") {
   }
